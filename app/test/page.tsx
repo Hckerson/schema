8d26173d2cf8f8d9fc17d5413d799d865741fb1c
Hckerson <166761968+Hckerson@users.noreply.github.com>
2025-08-23 13:57:02 +0100

    Update dependencies in package.json to improve project stability and performance.

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Test(){
+  return (
+    <main className="inset-0 flex items-center justify-center">
+      <span>
+        This is the test page
+      </span>
+    </main>
+  )
+}
