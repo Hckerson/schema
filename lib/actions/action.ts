b6e82555e6161f5207bbfb347500109217eb6472
Hckerson <166761968+Hckerson@users.noreply.github.com>
2025-08-24 00:42:56 +0100

    Comment out user deletion logic in deleteUser function for debugging purposes.

--- lib/actions/action.ts
+++ lib/actions/action.ts
@@ -4,8 +4,8 @@ import prisma from "@/prisma/connections";
 
 export const deleteUser = async (id: string) => {
   try {
-    const deletedUSer = await prisma.user.delete({where : {id : id}})
-    return deletedUSer;
+    // const deletedUSer = await prisma.user.delete({where : {id : id}})
+    // return deletedUSer;
   } catch (error) {
     console.log(error);
   }
